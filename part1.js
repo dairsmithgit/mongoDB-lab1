c7480b8eed71b5164defb6850865ea6799d10881
Dair Smith <dair.code@gmail.com>
2021-05-26 15:00:56 -0400

    part 1 of lab

---
+++ part1.js
@@ -0,0 +1,13 @@
+db.people.find();
+db.people.find().count();
+db.people.find( { state: "Arizona" });
+db.people.find( { state: "Arizona", gender: "Male" });
+db.people.find({ $or: [ {state: "Arizona"}, {state: "New Mexico"} ] });
+db.people.find({age: {$lt: 40} });
+db.people.find( { $and: [ {gender: "Female"}, {state: "Florida"}, {age: {$gte: 40}}, {age: {$lte: 45}}]});
+db.people.find( {first_name: /^H/});
+db.people.find( {state: "Michigan"} ).sort( {first_name: 1} );
+db.people.find( {$or: [ {state: "Virginia"}, {first_name: "Virginia"}] });
+db.people.find({age: {$lt: 30}}, {email: false, gender: false, age: false, state: false, children: false});
+db.people.find({state: "Montana"}, {age: false});
+db.people.find({email: /.edu$/}, {first_name: false, last_name: false, age: false, gender: false, state: false, children: false});
